188a7001979c8e03b0bcda91677e259fbc24039d
Godfrey Anthony T. Serojales <gaserojales@gmail.com>
2024-07-20 23:18:29 +0800

    gsap text

--- src/pages/GsapText.jsx
+++ src/pages/GsapText.jsx
@@ -1,5 +1,29 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
 const GsapText = () => {
   // TODO: Implement gsap text animation
+  useGSAP(() => {
+    gsap.to('#text', {
+      ease: 'power1.inOut',
+      opacity: 1,
+      y: 0,
+    });
+
+    gsap.fromTo(
+      '.para',
+      {
+        opacity: 0,
+        y: 20,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        delay: 1,
+        stagger: 0.1,
+      }
+    );
+  });
 
   return (
     <main>
@@ -8,8 +32,8 @@ const GsapText = () => {
       </h1>
 
       <p className="mt-5 text-gray-500 para">
-        We can use same method like <code>gsap.to()</code>,{" "}
-        <code>gsap.from()</code>, <code>gsap.fromTo()</code> and{" "}
+        We can use same method like <code>gsap.to()</code>,{' '}
+        <code>gsap.from()</code>, <code>gsap.fromTo()</code> and{' '}
         <code>gsap.timeline()</code> to animate text.
       </p>
 
@@ -25,14 +49,14 @@ const GsapText = () => {
       </p>
 
       <p className="mt-5 text-gray-500 para">
-        Read more about the{" "}
+        Read more about the{' '}
         <a
           href="https://greensock.com/docs/v3/Plugins/TextPlugin"
           target="_blank"
           rel="noreferrer noopener nofollow"
         >
           TextPlugin
-        </a>{" "}
+        </a>{' '}
         plugin.
       </p>
     </main>
